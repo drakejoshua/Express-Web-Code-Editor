4515c5d75f2cd6de664d45f0652eb1792b66ce86
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-04 00:38:10 +0100

    mark: added user and blok database schemas on the       backend
    - added the user schema for creation and tracking of
      users on the database/backend
    - added the blok schema for creation and tracking of
      bloks on the database/backend

---
+++ Backend/db/BlokSchema.js
@@ -0,0 +1,39 @@
+// import schema dependencies
+import mongoose from "mongoose";
+
+// Note: The BlokSchema is designed to store user-created code blocks
+// with HTML, CSS, and JavaScript content, along with user-specific settings
+// for the code editor. The user_id field links each block to a specific user.
+
+// create blok schema
+const BlokSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true
+    },
+    html: {
+        type: String
+    },
+    css: {
+        type: String
+    },
+    javascript: {
+        type: String
+    },
+    settings: {
+        type: {
+            theme: { type: String, required: true, default: "vs" },
+            font_size: { type: Number, default: 16, required: true },
+            tab_size: { type: Number, default: 4, required: true },
+            auto_complete: { type: Boolean, default: true, required: true },
+            editor_layout: { type: String, default: "top", required: true },
+        },
+        required: true
+
+    }
+})
+
+
+// export blok model for creation of blok documents
+// in the bloks collection on database
+export default mongoose.model("bloks", BlokSchema);
---
+++ Backend/db/UserSchema.js
@@ -0,0 +1,72 @@
+// import schema dependencies
+import mongoose from "mongoose";
+
+// Note: The UserSchema is designed to store user information, including
+// personal data, authentication metadata, and email verification status.
+// It includes fields for username, email, password, profile photo, and
+// various tokens for password reset, magic link authentication, and email verification.
+// The schema also tracks whether the user's email has been verified and the provider used for authentication.
+
+const UserSchema = new mongoose.Schema({
+    // personal user data
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6 // minimum of 6 characters for password
+    },
+    profile_photo_url: {
+        type: String
+    },
+    profile_photo_id: {
+        type: String
+    },
+
+    // auth metadata
+    reset_password_token: {
+        type: String
+    },
+    reset_password_expiry: {
+        type: Date,
+    },
+    magiclink_token: {
+        type: String
+    },
+    magiclink_expiry: {
+        type: Date,
+    },
+    verify_email_token: {
+        type: String
+    },
+    verify_email_expiry: {
+        type: Date,
+    },
+
+    // email verification and provider
+    email_verified: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    provider: {
+        type: String,
+        required: true
+    },
+
+    // api auth
+    api_key: {
+        type: String
+    },
+})
+
+
+// export user model for creation of user documents
+// in the users collection on database
+export default mongoose.model("users", UserSchema)
