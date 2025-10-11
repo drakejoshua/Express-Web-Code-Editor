e50c3f66d2cbfaaf8df0fcb5210b5f13a9f76890
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-11 20:43:15 +0100

    feat( routing ): add email confirmation/verification route
    - add the markup and styling for the email
      verification route
    
    This commit adds the email verification route markup and styles

--- Frontend/src/routes/Verify.jsx
+++ Frontend/src/routes/Verify.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
+// Email Verification Route
+// This route handles email confirmation and verification. 
+// It serves as a callback route to redirect users from their email
+// after they have clicked to confirm email.
+// The actual email token is sent to and handled on the backend, so 
+// this page just simply informs
+// the user that they will be redirected or display a loading/error indicator.
 
+// import route dependencies
+import RouteContainer from '../components/RouteContainer';
+import Logo from '../components/Logo';
+import StatusCard from '../components/StatusCard';
+import { Helmet } from 'react-helmet-async'
+import { FaArrowLeft, FaArrowRotateLeft, FaMoon, FaRegSun } from 'react-icons/fa6';
+import { useThemeProvider } from '../providers/ThemeProvider.jsx'
+
+
+// email verify component
 export default function Verify() {
-  return (
-    <div>
-      This is the Verify page.
-    </div>
-  )
+    const { theme, toggleTheme } = useThemeProvider();
+
+    return (
+        <>
+            <Helmet>
+                <title>Confirm Your Email - CodeBloks</title>
+                <meta name="description" content="Confirm the email for your account" />
+            </Helmet>
+
+            <div 
+                className='
+                    google 
+                    h-screen 
+                    w-full 
+                    bg-neutral-100 dark:bg-gray-900 
+                    dark:text-white 
+                    overflow-auto
+                '
+            >
+                
+                <RouteContainer>
+                    <Logo/>
+
+                    {/* <StatusCard
+                        status={{
+                            heading: 'Confirming your Email address...',
+                            text: `
+                                Please hold on while we confirm this email address 
+                                for your account. You will be redirected shortly.
+                            `,
+                            type: "loading"
+                        }}
+                    /> */}
+                    
+                    <StatusCard
+                        status={{
+                            heading: 'There was an issue confirming your email address.',
+                            text: `
+                                An error occurred while confirming your email address. 
+                                Please try again.
+                                If the issue persists, 
+                                please check your internet connection or contact support.
+                                Error: Invalid or expired token.
+                            `,
+                            type: "error",
+                            action: function() {
+                                alert("trying again")
+                            },
+                            action_content: <>
+                                <FaArrowRotateLeft/>
+                                Try Again
+                            </>
+                        }}
+                    />
+                    
+                    {/* <StatusCard
+                        status={{
+                            heading: 'Email Confirmed Successfully!',
+                            text: `
+                                You have successfully confirmed your email.
+                                You will be redirected to the dashboard shortly.
+                                If you are not redirected, please click the button below.
+                            `,
+                            type: "success",
+                            redirect: "/dashboard",
+                            redirect_content: <>
+                                Go to Dashboard
+                            </>
+                        }}
+                    /> */}
+                </RouteContainer>
+
+                <button 
+                    className='
+                        google--theme-toggle
+                        fixed
+                        top-4
+                        right-4
+                        flex
+                        gap-2
+                        items-center
+                        bg-gray-300 dark:bg-gray-700
+                        p-3 px-4.5
+                        rounded-lg
+                    '
+                    onClick={ () => toggleTheme() }
+                >
+                    { theme == 'dark' && <FaRegSun 
+                        className="
+                            google--theme-toggle__icon
+                        "
+                    /> }
+                    
+                    { theme == 'light' && <FaMoon 
+                        className="
+                            google--theme-toggle__icon
+                        "
+                    /> }
+
+                    <span 
+                        className="
+                            google--theme-toggle__text
+                        ">
+                        Toggle Theme
+                    </span>
+                </button>
+            </div>
+        </>
+    )
 }
