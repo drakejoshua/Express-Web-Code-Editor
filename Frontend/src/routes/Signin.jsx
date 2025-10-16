3b0556a6284e383c886e91bc9bab5842d3cc9f63
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-16 06:01:45 +0100

    fix( routing ): add page title content to signin route
    This commit enhances the Signin route by adding a descriptive title
    and meta description. This improves user experience and SEO by
    providing clear context about the page's purpose.

--- Frontend/src/routes/Signin.jsx
+++ Frontend/src/routes/Signin.jsx
@@ -6,138 +6,147 @@ import Logo from '../components/Logo'
 import EmailField from '../components/EmailField'
 import Carousel from '../components/Carousel'
 import RouteThemeToggle from '../components/RouteThemeToggle'
+import { Helmet } from 'react-helmet-async'
 
 
 export default function Signin() {
     return (
-        <div className='
-                signin
-                min-h-screen
-                overflow-auto
-                flex
-                justify-center
-                bg-white dark:bg-gray-900 
-                dark:text-white 
-            '
-        >
-            {/* signin form */}
-            <Form.Root 
-                className='
-                    signin--form
-                    w-2/3 lg:w-2/4 xl:w-1/3 min-w-[320px]
-                    p-4 py-12 xl:p-10 xl:py-16
+        <>
+            <Helmet>
+                <title> Sign In - Codebloks </title>
+                <meta name="description" content="Sign in to your Codebloks account 
+                to access the Express Web Code Editor and start coding!" />
+            </Helmet>
+        
+            <div className='
+                    signin
+                    min-h-screen
+                    overflow-auto
+                    flex
+                    justify-center
+                    bg-white dark:bg-gray-900 
+                    dark:text-white 
                 '
             >
-                {/* Form Logo */}
-                <Logo
+                {/* signin form */}
+                <Form.Root 
                     className='
-                        justify-center xl:justify-start
-                    '
-                />
-
-                {/* heading */}
-                <h1 className='
-                    signin--form__heading
-                    font-medium
-                    text-gray-900 dark:text-white
-                    text-3xl
-                    mt-9
-                    text-center xl:text-left
-                '>
-                    Sign in to your account
-                </h1>
-
-                {/* text */}
-                <p className='
-                    signin--form__text
-                    mt-4 mb-8
-                    text-lg
-                    leading-6
-                    text-center xl:text-left
-                '>
-                    Welcome back! Please enter your details to sign in to your account.
-                </p>
-
-                {/* email */}
-                <EmailField
-                    label="Email"
-                    name="email"
-                    emptyValidationMessage="Please enter your email"
-                    invalidValidationMessage="Please enter a valid email"
-                />
-                
-                {/* password */}
-                <PasswordField
-                    label="Password"
-                    name="password"
-                    emptyValidationMessage="Please enter your password"
-                    shortValidationMessage="The password can't be lower than 6 characters"
-                    className="
-                        mt-3.5
-                    "
-                />
-
-                {/* forgot password link */}
-                <a href="#" className='
-                    signin--form__forgot-password
-                    mt-4
-                    font-medium
-                    text-blue-900 dark:text-blue-100
-                    hover:underline
-                    block
-                    text-right
-                '>
-                    Forgot password?
-                </a>
-
-                {/* submit button */}
-                <Button 
-                    type="submit"
-                    className='
-                        w-full
-                        mt-6
+                        signin--form
+                        w-2/3 lg:w-2/4 xl:w-1/3 min-w-[320px]
+                        p-4 py-12 xl:p-10 xl:py-16
                     '
                 >
-                    Sign in
-                </Button>
+                    {/* Form Logo */}
+                    <Logo
+                        className='
+                            justify-center xl:justify-start
+                        '
+                    />
 
-                {/* sign in with google */}
-                <button
-                    className='
-                        signin--form__google-btn
+                    {/* heading */}
+                    <h1 className='
+                        signin--form__heading
+                        font-medium
+                        text-gray-900 dark:text-white
+                        text-3xl
+                        mt-9
+                        text-center xl:text-left
+                    '>
+                        Sign in to your account
+                    </h1>
+
+                    {/* text */}
+                    <p className='
+                        signin--form__text
+                        mt-4 mb-8
+                        text-lg
+                        leading-6
+                        text-center xl:text-left
+                    '>
+                        Welcome back! Please enter your details to sign in to your account.
+                    </p>
+
+                    {/* email */}
+                    <EmailField
+                        label="Email"
+                        name="email"
+                        emptyValidationMessage="Please enter your email"
+                        invalidValidationMessage="Please enter a valid email"
+                    />
+                    
+                    {/* password */}
+                    <PasswordField
+                        label="Password"
+                        name="password"
+                        emptyValidationMessage="Please enter your password"
+                        shortValidationMessage="The password can't be lower than 6 characters"
+                        className="
+                            mt-3.5
+                        "
+                    />
+
+                    {/* forgot password link */}
+                    <a href="#" className='
+                        signin--form__forgot-password
                         mt-4
-                        w-full
-                        flex
-                        items-center
-                        justify-center
-                        gap-3.5
-                        p-2.5
-                        rounded-md
-                        text-white
-                        bg-gray-800 hover:bg-gray-700 dark:bg-gray-600 hover:dark:bg-gray-500
+                        font-medium
+                        text-blue-900 dark:text-blue-100
+                        hover:underline
+                        block
+                        text-right
+                    '>
+                        Forgot password?
+                    </a>
+
+                    {/* submit button */}
+                    <Button 
+                        type="submit"
+                        className='
+                            w-full
+                            mt-6
+                        '
+                    >
+                        Sign in
+                    </Button>
+
+                    {/* sign in with google */}
+                    <button
+                        className='
+                            signin--form__google-btn
+                            mt-4
+                            w-full
+                            flex
+                            items-center
+                            justify-center
+                            gap-3.5
+                            p-2.5
+                            rounded-md
+                            text-white
+                            bg-gray-800 hover:bg-gray-700 dark:bg-gray-600 hover:dark:bg-gray-500
+                        '
+                    >
+                        <FaGoogle className='
+                            signin--form__google-btn--icon
+                            text-xl
+                        '/>
+
+                        Sign in with Google
+                    </button>
+                </Form.Root>
+
+                {/* signin carousel */}
+                <Carousel
+                    className='
+                        hidden xl:block
+                        w-2/3
+                        self-stretch 
+                        overflow-auto
                     '
-                >
-                    <FaGoogle className='
-                        signin--form__google-btn--icon
-                        text-xl
-                    '/>
+                />
 
-                    Sign in with Google
-                </button>
-            </Form.Root>
-
-            {/* signin carousel */}
-            <Carousel
-                className='
-                    hidden xl:block
-                    w-2/3
-                    self-stretch 
-                    overflow-auto
-                '
-            />
-
-            {/* theme toggle */}
-            <RouteThemeToggle />
-        </div>
+                {/* theme toggle */}
+                <RouteThemeToggle />
+            </div>
+        </>
     )
 }
