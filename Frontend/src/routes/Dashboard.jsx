359ae587aa42685ace25843734da67fd774284c8
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-20 21:32:45 +0100

    feat( routing ): add JSX markup for the Dashboard route
    This commit adds the jsx markup for the Dashboard route. The
    markup is added before styling is applied. This is part of the
    initial setup for the Dashboard component in the application.

--- Frontend/src/routes/Dashboard.jsx
+++ Frontend/src/routes/Dashboard.jsx
@@ -1,125 +1,244 @@
-import { SimpleCarousel } from '../components/simpleCarousel';
+// import route dependencies
+import { FaArrowRightFromBracket, FaArrowsRotate, FaBars, FaEllipsisVertical, FaMagnifyingGlass, FaMoon, FaPencil, FaPlus, FaTrash, FaUser } from 'react-icons/fa6'
+import Logo from '../components/Logo'
+import { Avatar, Dialog, DropdownMenu } from 'radix-ui'
+import Button from '../components/Button'
+
 
 export default function Dashboard() {
   return (
-    <div>
-        <SimpleCarousel.Root className="relative">
-            {/* --- Scroller --- */}
-            <SimpleCarousel.Scroller
-                className="
-                    overflow-x-auto
-                    scroll-smooth
-                    snap-x 
-                    snap-mandatory
-                    h-[80vh]
-                "
-            >
-                {/* --- Track --- */}
-                <SimpleCarousel.Track
-                    className="
-                        flex
-                        transition-transform
-                        duration-500
-                        ease-in-out
-                    "
-                >
-                    <SimpleCarousel.Item
-                        className="
-                            flex-[0_0_100%]
-                            snap-start
-                        "
-                    >
-                        <img
-                            src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.1.0&auto=format&fit=crop"
-                            alt="Image 1"
-                            className="block w-full h-full object-cover"
+    <div
+        className='
+            dashboard
+        '
+    >
+        <div className="dashboard--header">
+            <Dialog.Root>
+                <Dialog.Trigger className='dashboard--header__menu-trigger'>
+                    <FaBars className='dashboard--header__menu-trigger-icon' />
+                </Dialog.Trigger>
+
+                <Dialog.Portal>
+                    <Dialog.Content>
+                        <Logo className='dashboard--header__menu-logo' />
+
+                        <div className="dashboard--header__menu-search-input-ctn">
+                            <FaMagnifyingGlass className='dashboard--header__search-icon' />
+
+                            <input 
+                                type="text" 
+                                className='dashboard--header__search-input'
+                            />
+                        </div>
+
+                        <div className="dashboard--header__menu-blok-list">
+                            <div className="dashboard--header__menu-blok">
+                                <span className="dashboard--header__blok-name">
+                                    blok_1
+                                </span>
+
+                                <DropdownMenu.Root>
+                                    <DropdownMenu.Trigger asChild>
+                                        <button className="dashboard--header__blok-trigger">
+                                            <FaEllipsisVertical className="dashboard--header__blok-trigger-icon"/>
+                                        </button>
+                                    </DropdownMenu.Trigger>
+
+                                    <DropdownMenu.Portal>
+                                        <DropdownMenu.Content className='dashboard--header__blok-options'>
+                                            <DropdownMenu.Label className='dashboard--header__blok-options-label'>
+                                                more options
+                                            </DropdownMenu.Label>
+
+                                            <DropdownMenu.Item>
+                                                <FaPencil className='dashboard--header__blok-option-icon' />
+
+                                                <span className="dashboard--header__blok-option-text">
+                                                    rename blok
+                                                </span>
+                                            </DropdownMenu.Item>
+                                            
+                                            <DropdownMenu.Item>
+                                                <FaTrash className='dashboard--header__blok-option-icon' />
+
+                                                <span className="dashboard--header__blok-option-text">
+                                                    delete blok
+                                                </span>
+                                            </DropdownMenu.Item>
+                                        </DropdownMenu.Content>
+                                    </DropdownMenu.Portal>
+                                </DropdownMenu.Root>
+                            </div>
+                        </div>
+
+                        <button className="dashboard--header__load-more-btn">
+                            load more
+                        </button>
+
+                        <Button>
+                            <FaPlus/>
+
+                            <span>
+                                add new blok
+                            </span>
+                        </Button>
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
+
+            <div className="dashboard--header__title-ctn">
+                <Logo className='dashboard--header__logo' />
+
+                <h1 className="dashboard--header__title">
+                    Welcome, Joshua
+                </h1>
+            </div>
+
+            <button className="dashboard--header__theme-toggle-btn">
+                <FaMoon className='dashboard--header__theme-toggle-icon' />
+            </button>
+
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                    <Avatar.Root className='dashboard--header__avatar-ctn'>
+                        <Avatar.Image 
+                            src={'https://images.unsplash.com/photo-1598096969068-7f52cac10c83?ixlib=rb-4.1.0'
+                            +'&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Ym95JTIwcG9ydHJhaXR8ZW58MHx8MHx8fDA%3D'}
+                            className='dashboard--header__avatar-photo'
                         />
-                    </SimpleCarousel.Item>
+                        
+                        <Avatar.Fallback delayMs={3000}>
+                            <FaUser className='dashboard--header__avatar-icon'/>
+                        </Avatar.Fallback>
+                    </Avatar.Root>
+                </DropdownMenu.Trigger>
 
-                    <SimpleCarousel.Item
-                        className="
-                            flex-[0_0_100%]
-                            snap-start
-                        "
-                    >
-                        <img
-                            src="https://images.unsplash.com/photo-1471039497385-b6d6ba609f9c?ixlib=rb-4.1.0&auto=format&fit=crop"
-                            alt="Image 2"
-                            className="block w-full h-full object-cover"
-                        />
-                    </SimpleCarousel.Item>
+                <DropdownMenu.Portal>
+                    <DropdownMenu.Content>
+                        <div className="dashboard--header__profile-summary">
+                            <Avatar.Root className='dashboard--header__avatar-ctn'>
+                                <Avatar.Image 
+                                    src={'https://images.unsplash.com/photo-1598096969068-7f52cac10c83?ixlib=rb-4.1.0'
+                                    +'&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Ym95JTIwcG9ydHJhaXR8ZW58MHx8MHx8fDA%3D'}
+                                    className='dashboard--header__avatar-photo'
+                                />
+                                
+                                <Avatar.Fallback delayMs={3000}>
+                                    <FaUser className='dashboard--header__avatar-icon'/>
+                                </Avatar.Fallback>
+                            </Avatar.Root>
 
-                    <SimpleCarousel.Item
-                        className="
-                            flex-[0_0_100%]
-                            snap-start
-                        "
-                    >
-                        <img
-                            src="https://images.unsplash.com/photo-1487958449943-2429e8be8625?ixlib=rb-4.1.0&auto=format&fit=crop"
-                            alt="Image 3"
-                            className="block w-full h-full object-cover"
-                        />
-                    </SimpleCarousel.Item>
-                </SimpleCarousel.Track>
-            </SimpleCarousel.Scroller>
+                            <div className='dashboard--header__profile-info'>
+                                <span className="dashboard--header__profile-username">
+                                    Joshua Mabawonku
+                                </span>
+                                
+                                <span className="dashboard--header__profile-email">
+                                    joshua@email.com
+                                </span>
+                            </div>
+                        </div>
 
-            {/* --- Navigation Buttons --- */}
-            <SimpleCarousel.Button
-                direction="prev"
-                className="
-                    absolute
-                    top-1/2
-                    -translate-y-1/2
-                    left-4
-                    bg-white
-                    px-2
-                    py-1
-                    rounded
-                "
-            >
-                prev
-            </SimpleCarousel.Button>
+                        <div className="dashboard--header__profile-options-list">
+                            <DropdownMenu.Item className='dashboard--header__profile-option'>
+                                <FaArrowsRotate className="dashboard--header__option-icon" /> 
 
-            <SimpleCarousel.Button
-                direction="next"
-                className="
-                    absolute
-                    top-1/2
-                    -translate-y-1/2
-                    right-4
-                    bg-white
-                    px-2
-                    py-1
-                    rounded
-                "
-            >
-                next
-            </SimpleCarousel.Button>
+                                <span className="dashboard--header__option-text">
+                                    Update Profile
+                                </span>
+                            </DropdownMenu.Item>
+                            
+                            <DropdownMenu.Item className='dashboard--header__profile-option'>
+                                <FaArrowRightFromBracket className="dashboard--header__option-icon" /> 
 
-            {/* --- Tabs (Indicators) --- */}
-            <SimpleCarousel.Tabs
-                className="
-                    flex
-                    justify-center
-                    gap-2
-                    mt-4
-                "
-            >
-                {({ index, isSelected }) => (
-                    <SimpleCarousel.Tab
-                        key={index}
-                        index={index}
-                        className={`
-                            w-4
-                            h-4
-                            rounded-full
-                            ${isSelected ? 'bg-gray-600' : 'bg-gray-300'}
-                        `}
-                    />
-                )}
-            </SimpleCarousel.Tabs>
-        </SimpleCarousel.Root>
+                                <span className="dashboard--header__option-text">
+                                    logout
+                                </span>
+                            </DropdownMenu.Item>
+                        </div>
+                    </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+            </DropdownMenu.Root>
+        </div>
+
+        <div className="dashboard--filter-bar">
+            <div className="dashboard--filter-bar__search-input-ctn">
+                <FaMagnifyingGlass className='dashboard--filter-bar__search-icon' />
+
+                <input 
+                    type="text" 
+                    className='dashboard--filter-bar__search-input'
+                />
+            </div>
+
+            <Button>
+                <FaPlus className='dashboard--filter-bar__add-btn-icon' />
+
+                <span className="dashboard--filter-bar__add-btn-text">
+                    Create Blok
+                </span>
+            </Button>
+        </div>
+
+        <div className="dashboard--blok-list">
+            <div className="dashboard--blok-list__blok">
+                <iframe 
+                    srcDoc={`<body class="text-white bg-black">
+                        <!-- Tailwind CDN -->
+                        <script src="https://cdn.tailwindcss.com"></script>
+
+                        <h1 class="text-2xl font-medium mb-2">Welcome !!!</h1>
+                        <p class="text-gray-300 mb-8">
+                            Experience code-editing in the browser
+                        </p>
+
+                        <button class="bg-blue-500 text-white px-3 py-1.5 rounded">
+                            Only Available At Codebloks
+                        </button>
+                    </body>`}
+                    frameborder="0" 
+                    className="dashboard--blok-list__blok-preview"
+                ></iframe>
+
+                <div className="dashboard--blok-list__blok-details">
+                    <span className="dashboard--blok-list__blok-name">
+                        blok_1
+                    </span>
+
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger asChild>
+                            <button className="dashboard--blok-list__blok-trigger">
+                                <FaEllipsisVertical className="dashboard--blok-list__blok-trigger-icon"/>
+                            </button>
+                        </DropdownMenu.Trigger>
+
+                        <DropdownMenu.Portal>
+                            <DropdownMenu.Content className='dashboard--blok-list__blok-options'>
+                                <DropdownMenu.Label className='dashboard--blok-list__blok-options-label'>
+                                    more options
+                                </DropdownMenu.Label>
+
+                                <DropdownMenu.Item>
+                                    <FaPencil className='dashboard--blok-list__blok-option-icon' />
+
+                                    <span className="dashboard--blok-list__blok-option-text">
+                                        rename blok
+                                    </span>
+                                </DropdownMenu.Item>
+                                
+                                <DropdownMenu.Item>
+                                    <FaTrash className='dashboard--blok-list__blok-option-icon' />
+
+                                    <span className="dashboard--blok-list__blok-option-text">
+                                        delete blok
+                                    </span>
+                                </DropdownMenu.Item>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Portal>
+                    </DropdownMenu.Root>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
