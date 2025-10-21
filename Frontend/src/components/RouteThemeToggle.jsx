6796f20245465a389abd38ba57f3463b73a4d9b5
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-21 16:34:36 +0100

    docs( component ): add internal comments in RouteThemeToggle component for better code clarity and maintainability
    This commit adds descriptive comments to the RouteThemeToggle.jsx
    component file. The comments explain the purpose of the component,
    its props, and key sections of the code. This improves code readability
    and helps future developers understand the functionality and structure
    of the component.

--- Frontend/src/components/RouteThemeToggle.jsx
+++ Frontend/src/components/RouteThemeToggle.jsx
@@ -1,3 +1,10 @@
+// RouteThemeToggle.jsx
+// This component defines a theme toggle button used in non-user-facing
+// routes such as password reset, email verification, etc. to switch between
+// light and dark themes.
+
+
+
 // import component dependencies
 import { FaRegSun, FaMoon } from 'react-icons/fa6'
 import { useThemeProvider } from '../providers/ThemeProvider';
@@ -5,10 +12,13 @@ import { useThemeProvider } from '../providers/ThemeProvider';
 
 // define RouteThemeToggle component
 export default function RouteThemeToggle() {
+    // access theme and toggle function from the ThemeProvider 
+    // context
     const { theme, toggleTheme } = useThemeProvider();
 
     return (
         <button 
+            // apply default classes for the theme toggle button
             className='
                 theme-toggle
                 fixed
@@ -21,14 +31,17 @@ export default function RouteThemeToggle() {
                 p-3 px-4.5
                 rounded-lg
             '
+            // toggle theme on button click using onclick handler
             onClick={ () => toggleTheme() }
         >
+            {/* show sun icon when theme is dark */}
             { theme == 'dark' && <FaRegSun 
                 className="
                     theme-toggle__icon
                 "
             /> }
-            
+
+            {/* show moon icon when theme is light */}
             { theme == 'light' && <FaMoon 
                 className="
                     theme-toggle__icon
