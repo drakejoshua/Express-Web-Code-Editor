66fb3a8998b3f31be9bd55444089b1c59a2541c5
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-22 15:28:46 +0100

    docs( provider ): add internal comment to ThemeProvider for clarity and maintainability
    This commit adds descriptive comments to the ThemeProvider component
    to explain its purpose, functionality, and usage of context and hooks. This
    enhancement improves code readability and helps future developers
    understand the implementation details of the ThemeProvider.

--- Frontend/src/providers/ThemeProvider.jsx
+++ Frontend/src/providers/ThemeProvider.jsx
@@ -1,17 +1,25 @@
+// import provider dependencies
 import { createContext, useContext } from 'react'
 import useTheme from '../hooks/useTheme.js'
 
+// create context for sharing theme state and toggle function
+// across the application
 const ThemeContext = createContext()
 
+// custom hook to access theme context from ThemeProvider
 export function useThemeProvider() {
   return useContext(ThemeContext)
 }
 
 function ThemeProvider({ children }) {
+    // use custom useTheme hook to initialize current theme and toggle function
+    // from local storage
     const [ theme, toggleTheme ] = useTheme("codebloks-app-theme")
 
     return (
+        // provide theme state and toggle function to children components
         <ThemeContext.Provider value={ { theme, toggleTheme } }>
+            {/* add tailwind "dark" class to root div for toggling dark mode */}
             <div className={ ( theme == "dark" ) ? "dark" : "" }>
                 { children }
             </div>
