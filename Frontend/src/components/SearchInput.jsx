84e8023c116603e49a0ad07a97af3fad7be3f040
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-23 18:16:42 +0100

    docs( component ): add comments to SearchInput component for clarity and maintainability
    This commit adds descriptive comments to the SearchInput component
    to explain its purpose, functionality, and key elements. The comments
    aim to enhance code readability and assist future developers in
    understanding the component's structure and behavior.

--- Frontend/src/components/SearchInput.jsx
+++ Frontend/src/components/SearchInput.jsx
@@ -1,36 +1,44 @@
-import React from 'react'
+// SearchInput.jsx
+// This component renders a search input field with a search icon.
+// It accepts additional class names and props to customize its behavior and appearance.
+
+// import component dependencies
 import { FaMagnifyingGlass } from 'react-icons/fa6'
 
-export default function SearchInput({ className, ...props }) {
-  return (
-    <div 
-        className={`
-            search-input-ctn
-            bg-neutral-200
-            rounded-lg
-            flex
-            p-2 px-4
-            items-center
-            gap-3
-            *:text-gray-800
-            ${ className || "" }
-        `}
-    >
-        <FaMagnifyingGlass className='search-input-ctn__search-icon' />
 
-        <input 
-            type="text" 
-            className='
-                search-input-ctn__search-input
-                placeholder-shown:capitalize
-                outline-none
-                font-medium
-                flex-1
-                min-w-0
-            '
-            placeholder='search your bloks...'
-            { ...props }
-        />
-    </div>
-  )
+// define SearchInput component
+export default function SearchInput({ className, ...props }) {
+    return (
+        <div 
+            // apply default and custom classes
+            className={`
+                search-input-ctn
+                bg-neutral-200
+                rounded-lg
+                flex
+                p-2 px-4
+                items-center
+                gap-3
+                *:text-gray-800
+                ${ className || "" }
+            `}
+        >
+            <FaMagnifyingGlass className='search-input-ctn__search-icon' />
+
+            {/* text input for collecting search queries */}
+            <input 
+                type="text" 
+                className='
+                    search-input-ctn__search-input
+                    placeholder-shown:capitalize
+                    outline-none
+                    font-medium
+                    flex-1
+                    min-w-0
+                '
+                placeholder='search your bloks...'
+                { ...props }
+            />
+        </div>
+    )
 }
