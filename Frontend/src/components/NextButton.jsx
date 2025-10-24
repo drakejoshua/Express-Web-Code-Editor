c91a22e8fb9bbdfd4b22c29b600085b33a50f350
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-24 22:07:49 +0100

    docs( component ): add comments to NextButton component for clarity and maintainability
    This commit adds descriptive comments to the NextButton component
    to enhance code readability and assist future developers in understanding
    its purpose and functionality. The comments provide context on the component's
    usage within the application.

--- Frontend/src/components/NextButton.jsx
+++ Frontend/src/components/NextButton.jsx
@@ -1,9 +1,14 @@
+// import component dependencies
 import { forwardRef } from "react"
 import Button from "./Button"
 
+
+// define NextButton component
 const NextButton = forwardRef(({children, className, ...props }, ref) => {
     return <Button 
                 type="button"
+
+                // apply default and custom classes to the button
                 className={`
                     flex-grow
                     ${ className || "" }
@@ -16,4 +21,6 @@ const NextButton = forwardRef(({children, className, ...props }, ref) => {
 })
 
 
+// export NextButton component for use in 
+// other parts of the application
 export default NextButton
