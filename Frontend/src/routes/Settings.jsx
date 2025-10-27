967f5524dbb4773fe4df7205fd7e1d3861a4ba5b
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-10-27 19:07:39 +0100

    feat( styling ): add route and scrollspy styling for the settings route
    This commit add the styling to settings route and it's internal scrollspy
    improving the UX and appearance of the layout.
    This change prepares the settings route for theming and responsive design.

--- Frontend/src/routes/Settings.jsx
+++ Frontend/src/routes/Settings.jsx
@@ -1,4 +1,4 @@
-import { FaArrowLeft } from "react-icons/fa6";
+import { FaArrowLeftLong, FaMoon, FaPen, FaPencil, FaRegSun, FaTrash } from "react-icons/fa6";
 import WideLayout from "../components/WideLayout";
 import { DropdownMenu, Form } from "radix-ui";
 import TextField from "../components/TextField";
@@ -7,118 +7,256 @@ import PasswordField from "../components/PasswordField";
 import UserAvatar from "../components/UserAvatar";
 import FinishButton from "../components/FinishButton";
 import ScrollSpy from "react-ui-scrollspy";
+import { useRef } from "react";
 
 export default function Settings() {
-  return (
-    <WideLayout>
-        <div
-            className="
-                settings
-            "
-        >
-            <div className="settings--header">
-                <FaArrowLeft className="settings--header__icon"/>
-
-                <h1 className="settings--header__text">
+    const parentRef = useRef(null)
+    
+    return (
+        <WideLayout>
+            <div
+                className="
                     settings
-                </h1>
-            </div>
-
-            <div className="settings--options-ctn">
+                    mt-8
+                    h-[90%]
+                    gap-8
+                    flex
+                    flex-col
+                "
+            >
                 <div 
                     className="
-                        settings--options-ctn__navbar
-                        [&>.active-scroll-spy]:bg-gray-400 dark:[&>.active-scroll-spy]:bg-gray-700
+                        settings--header
+                        flex
+                        items-center
+                        gap-6
+                        text-3xl
+                        capitalize
                     "
                 >
+                    <FaArrowLeftLong className="settings--header__icon" />
+
+                    <h1 className="settings--header__text">
+                        settings
+                    </h1>
+
                     <button 
-                        className="settings--options-ctn__nav-item"
-                        data-to-scrollspy-id="account"
+                        className="
+                            settings--header__theme-toggle
+                            ml-auto
+                            text-2xl
+                        "
                     >
-                        Account
-                    </button>
-                    
-                    <button 
-                        className="settings--options-ctn__nav-item"
-                        data-to-scrollspy-id="api"
-                    >
-                        API key
+                        <FaMoon className="settings--header__theme-toggle-icon"/>
+                        {/* <FaRegSun className="settings--header__theme-toggle-icon"/> */}
                     </button>
                 </div>
 
-                <div className="settings--options-ctn__content">
-                    <ScrollSpy>
-                        <div 
-                            className="settings--options-ctn__account-content"
-                            id="account"
+                <div 
+                    className="
+                        h-full
+                        settings--options-ctn
+                        grid
+                        grid-cols-[100%] lg:grid-cols-[18%_75%]
+                        items-stretch
+                        gap-8
+                        flex-grow
+                    "
+                >
+                    <div 
+                        className="
+                            settings--options-ctn__navbar
+                            flex
+                            flex-col
+                            items-stretch
+                            gap-1
+                            border-r-2 border-gray-300
+                            p-4
+
+                            *:text-left
+                            *:p-2 *:px-4
+                            *:rounded-md
+                            *:outline-none
+                            *:hover:bg-gray-200 dark:*:hover:bg-gray-800
+                            *:cursor-pointer
+                            *:tracking-wide
+                            [&>.active-scroll-spy]:bg-blue-600 dark:[&>.active-scroll-spy]:bg-gray-700
+                            [&>.active-scroll-spy]:text-white
+                        "
+                    >
+                        <button 
+                            className="settings--options-ctn__nav-item"
+                            data-to-scrollspy-id="account"
                         >
-                            <h2 className="settings--options-ctn__content-heading">
-                                Update Account Information
-                            </h2>
+                            <a href="#account">Account</a>
+                        </button>
+                        
+                        <button 
+                            className="settings--options-ctn__nav-item"
+                            data-to-scrollspy-id="api"
+                        >
+                            <a href="#api">API key</a>
+                        </button>
+                    </div>
+
+                    <div 
+                        className="
+                            settings--options-ctn__content
+                            h-full
+                            *:h-full
+                            overflow-auto
+                            pt-4
                             
-                            <p className="settings--options-ctn__content-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident?
-                            </p>
 
-                            <DropdownMenu.Root>
-                                <DropdownMenu.Trigger asChild>
-                                    <UserAvatar/>
-                                </DropdownMenu.Trigger>
-                                <DropdownMenu.Portal>
-                                    <DropdownMenu.Content>
-                                        <DropdownMenu.Item>
-                                            Delete Photo
-                                        </DropdownMenu.Item>
-                                        
-                                        <DropdownMenu.Item>
-                                            Update Photo
-                                        </DropdownMenu.Item>
-                                    </DropdownMenu.Content>
-                                </DropdownMenu.Portal>
-                            </DropdownMenu.Root>
+                            [&_.settings--options-ctn\_\_settings]:h-full
+                            [&_.settings--options-ctn\_\_settings]:w-1/2
 
-                            <Form.Root>
-                                <TextField
-                                    label="Username:"
-                                />
-
-                                <EmailField
-                                    label="Email:"
-                                />
-
-                                <PasswordField
-                                    label="Password:"
-                                />
-
-                                <FinishButton>
-                                    Update Profile
-                                </FinishButton>
-                            </Form.Root>
-                        </div>
-
-                        <div 
-                            className="settings--options-ctn__api-content"
-                            id="api"
+                            [&_.settings--options-ctn\_\_settings-heading]:text-2xl
+                            [&_.settings--options-ctn\_\_settings-heading]:mb-2
+                            
+                            [&_.settings--options-ctn\_\_settings-text]:mb-8
+                            
+                            [&_.settings--options-ctn\_\_settings-form]:flex
+                            [&_.settings--options-ctn\_\_settings-form]:flex-col
+                            [&_.settings--options-ctn\_\_settings-form]:gap-2
+                            
+                            [&_.settings--options-ctn\_\_settings-submit-btn]:mt-4
+                        "
+                        ref={parentRef}
+                    >
+                        <ScrollSpy
+                            parentScrollContainerRef={parentRef}
                         >
-                            <Form.Root>
-                                <PasswordField
-                                    label="API Key"
-                                    value="jsn3j4njk3jn34jnj"
-                                />
+                            <div 
+                                className="
+                                    settings--options-ctn__settings
+                                    h-full
+                                "
+                                id="account"
+                            >
+                                <h2 className="settings--options-ctn__settings-heading">
+                                    Update Account Information
+                                </h2>
+                                
+                                <p className="settings--options-ctn__settings-text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident?
+                                </p>
 
-                                <a href="#" className="settings--options-ctn__help-link">
-                                    Want to use this API key? Learn how to integrate it
-                                </a>
+                                <DropdownMenu.Root>
+                                    <DropdownMenu.Trigger asChild>
+                                        <UserAvatar
+                                            className="
+                                                h-28
+                                                w-28
+                                                mb-8
+                                            "
+                                        />
+                                    </DropdownMenu.Trigger>
+                                    <DropdownMenu.Portal>
+                                        <DropdownMenu.Content
+                                            className="
+                                                bg-gray-500
+                                                p-3.5
+                                                rounded-md
 
-                                <FinishButton>
-                                    Generate API Key
-                                </FinishButton>
-                            </Form.Root>
-                        </div>
-                    </ScrollSpy>
+                                                *:p-2 *:px-3
+                                                *:rounded-md
+                                                *:text-white
+                                                *:font-medium
+                                                *:hover:bg-gray-400
+                                                *:cursor-pointer
+                                                *:flex
+                                                *:gap-2
+                                                *:items-center
+                                            "
+                                            side="right"
+                                            sideOffset={12}
+                                            align="center"
+                                        >
+                                            <DropdownMenu.Item>
+                                                <FaTrash/>
+
+                                                <span>Delete Photo</span>
+                                            </DropdownMenu.Item>
+                                            
+                                            <DropdownMenu.Item>
+                                                <FaPen/>
+
+                                                <span>Update Photo</span>
+                                            </DropdownMenu.Item>
+                                        </DropdownMenu.Content>
+                                    </DropdownMenu.Portal>
+                                </DropdownMenu.Root>
+
+                                <Form.Root
+                                    className="
+                                        settings--options-ctn__settings-form
+                                    "
+                                >
+                                    <TextField
+                                        label="Username:"
+                                    />
+
+                                    <EmailField
+                                        label="Email:"
+                                    />
+
+                                    <PasswordField
+                                        label="Password:"
+                                    />
+
+                                    <FinishButton
+                                        className="
+                                            settings--options-ctn__settings-submit-btn
+                                        "
+                                    >
+                                        Update Profile
+                                    </FinishButton>
+                                </Form.Root>
+                            </div>
+
+                            <div 
+                                className="
+                                    settings--options-ctn__settings
+                                    h-full
+                                "
+                                id="api"
+                            >
+                                <h2 className="settings--options-ctn__settings-heading">
+                                    Your API Key
+                                </h2>
+                                
+                                <p className="settings--options-ctn__settings-text">
+                                    Retrieve or generate your api key 
+                                </p>
+
+                                <Form.Root
+                                    className="
+                                        settings--options-ctn__settings-form
+                                    "
+                                >
+                                    <PasswordField
+                                        label="API Key"
+                                        value="jsn3j4njk3jn34jnj"
+                                    />
+
+                                    <a href="#" className="settings--options-ctn__help-link">
+                                        Want to use this API key? Learn how to integrate it
+                                    </a>
+
+                                    <FinishButton
+                                        className="
+                                            settings--options-ctn__settings-submit-btn
+                                        "
+                                    >
+                                        Generate API Key
+                                    </FinishButton>
+                                </Form.Root>
+                            </div>
+                        </ScrollSpy>
+                    </div>
                 </div>
             </div>
-        </div>
-    </WideLayout>
-  )
+        </WideLayout>
+    )
 }
