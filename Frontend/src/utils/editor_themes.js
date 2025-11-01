aa6d8947e544989ad2c10122e3c3a93f723cfb5c
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-11-01 15:54:41 +0100

    chore( theme ): update editor_themes definitions with new themes and actual JSON imports
    This commit updates the editor_themes.js file to include the actual
    JSON theme imports for the Sunburst, Clouds, Github Dark, Monokai,
    Night Owl, and Solarized Dark themes. This change ensures that the
    themes are properly defined and can be utilized within the code editor.

--- Frontend/src/utils/editor_themes.js
+++ Frontend/src/utils/editor_themes.js
@@ -1,4 +1,17 @@
+// import util depdendencies
+import githubLight from "monaco-themes/themes/GitHub Light.json"
+import solarizedLight from "monaco-themes/themes/Solarized-light.json"
+import sunburst from "monaco-themes/themes/Sunburst.json"
+import clouds from "monaco-themes/themes/Clouds.json"
+import githubDark from "monaco-themes/themes/Github Dark.json"
+import monokai from "monaco-themes/themes/Monokai.json"
+import nightOwl from "monaco-themes/themes/Night Owl.json"
+import solarizedDark from "monaco-themes/themes/Solarized-dark.json"
+
+
+
 export const editorThemes = [
+    // light themes
     {
         type: "light",
         value: "vsc_light",
@@ -8,15 +21,29 @@ export const editorThemes = [
     {
         type: "light",
         value: "github_light",
-        json: "",
+        json: githubLight,
         name: "Github Light"
     },
     {
         type: "light",
-        value: "hc_light",
-        json: "",
-        name: "High Contrast Light"
+        value: "solarized_light",
+        json: solarizedLight,
+        name: "Solarized Light"
     },
+    {
+        type: "light",
+        value: "sunburst",
+        json: sunburst,
+        name: "Sunburst"
+    },
+    {
+        type: "light",
+        value: "clouds",
+        json: clouds,
+        name: "Clouds"
+    },
+
+    // dark themes
     {
         type: "dark",
         value: "vsc_dark",
@@ -26,13 +53,25 @@ export const editorThemes = [
     {
         type: "dark",
         value: "github_dark",
-        json: "",
+        json: githubDark,
         name: "Github Dark"
     },
     {
         type: "dark",
-        value: "hc_dark",
-        json: "",
-        name: "High Contrast Dark"
+        value: "monokai",
+        json: monokai,
+        name: "Monokai"
+    },
+    {
+        type: "dark",
+        value: "night_owl",
+        json: nightOwl,
+        name: "Night Owl"
+    },
+    {
+        type: "dark",
+        value: "solarized_dark",
+        json: solarizedDark,
+        name: "Solarized Dark"
     },
 ]
