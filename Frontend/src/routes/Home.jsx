711236aa98f0e50e1b01ae0b977df4411a2cf52f
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-11-05 17:18:52 +0100

    chore( assets ): add client logos and hero image, commence home route modifications
    This commit introduces new visual assets to enhance the user
    interface, including a set of client logos and a hero image.
    Additionally, initial modifications to the Home route have
    been made to integrate these assets and improve overall layout and design.

--- Frontend/src/routes/Home.jsx
+++ Frontend/src/routes/Home.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import Logo from '../components/Logo'
+import Button from '../components/Button'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-  return (
-    <div>
-      This is the home/landing page.
-    </div>
-  )
+    return (
+        <div
+            className='
+                home 
+                h-screen 
+                w-full
+            '
+        >
+            <div className="home--navbar">
+                <Logo />
+
+                <div className="home--nav-links">
+                    <a href="">
+                        about
+                    </a>
+
+                    <a href="">
+                        features
+                    </a>
+
+                    <Button>
+                        sign up
+                    </Button>
+
+                    <Link>
+                        sign in
+                    </Link>
+                </div>
+            </div>
+
+            <div className="home--hero">
+                <h3>
+                    the ultimate web code editor
+                </h3>
+
+                <h1>
+                    Experience the future of web code editing
+                </h1>
+
+                <p>
+                    CodeBloks is a powerful web code editor designed to streamline your development workflow.
+                    With an intuitive interface, real-time collaboration, and seamless deployment options.
+                </p>
+
+                <img src="" alt="" />
+            </div>
+
+            <div className="home--clients-marquee">
+                
+            </div>
+        </div>
+    )
 }
